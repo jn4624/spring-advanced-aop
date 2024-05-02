60ddb9bf8f4d2346abe2aaa37df85d4e324a6e25
LimJiNa <jn4624@gmail.com>
2024-05-02 16:43:44 +0900

    스프링 AOP 구현 - 어드바이스 종류 실행 시점 확인

---
+++ src/main/java/hello/aop/order/aop/AspectV6Advice.java
@@ -0,0 +1,57 @@
+package hello.aop.order.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+
+@Slf4j
+@Aspect
+public class AspectV6Advice {
+    /**
+     * JoinPoint 인터페이스 주요 기능
+     * - getArgs(): 메서드 인수 반환
+     * - getThis(): 프록시 객체 반환
+     * - getTarget(): 대상 객체 반환
+     * - getSignature(): 조언되는 메서드에 대한 설명 반환
+     * - toString(): 조언되는 방법에 대한 유용한 설명 인쇄
+     *
+     * ProceedingJoinPoint 인터페이스 주요 기능
+     * - proceed(): 다음 어드바이스나 타겟 호출
+     */
+
+    @Around("hello.aop.order.aop.Pointcuts.orderAndService()")
+    public Object doTransaction(ProceedingJoinPoint joinPoint) throws Throwable {
+        try {
+            log.info("[트랜잭션 시작] {}", joinPoint.getSignature());
+            Object result = joinPoint.proceed();
+            log.info("[트랜잭션 커밋] {}", joinPoint.getSignature());
+            return result;
+        } catch (Exception e) {
+            log.info("[트랜잭션 롤백] {}", joinPoint.getSignature());
+            throw e;
+        } finally {
+            log.info("[트랜잭션 릴리즈] {}", joinPoint.getSignature());
+        }
+    }
+
+    @Before("hello.aop.order.aop.Pointcuts.orderAndService()")
+    public void doBefore(JoinPoint joinPoint) {
+        log.info("[before] {}", joinPoint.getSignature());
+    }
+
+    @AfterReturning(value = "hello.aop.order.aop.Pointcuts.orderAndService()", returning = "result")
+    public void doReturn(JoinPoint joinPoint, Object result) {
+        log.info("[return] {}, return = {}", joinPoint.getSignature(), result);
+    }
+
+    @AfterThrowing(value = "hello.aop.order.aop.Pointcuts.orderAndService()", throwing = "ex")
+    public void doThrowing(JoinPoint joinPoint, Exception ex) {
+        log.info("[ex] {}, message = {}", joinPoint.getSignature(), ex.getMessage());
+    }
+
+    @After("hello.aop.order.aop.Pointcuts.orderAndService()")
+    public void doAfter(JoinPoint joinPoint) {
+        log.info("[after] {}", joinPoint.getSignature());
+    }
+}
