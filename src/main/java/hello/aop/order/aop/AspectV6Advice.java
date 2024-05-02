5755d08ef639449abec99dd2d19699cbb810d105
LimJiNa <jn4624@gmail.com>
2024-05-02 23:26:43 +0900

    스프링 AOP 구현 - 어드바이스 종류 실행 시점 주석 추가

--- src/main/java/hello/aop/order/aop/AspectV6Advice.java
+++ src/main/java/hello/aop/order/aop/AspectV6Advice.java
@@ -23,14 +23,18 @@ public class AspectV6Advice {
     @Around("hello.aop.order.aop.Pointcuts.orderAndService()")
     public Object doTransaction(ProceedingJoinPoint joinPoint) throws Throwable {
         try {
+            // @Before
             log.info("[트랜잭션 시작] {}", joinPoint.getSignature());
             Object result = joinPoint.proceed();
+            // @AfterReturning
             log.info("[트랜잭션 커밋] {}", joinPoint.getSignature());
             return result;
         } catch (Exception e) {
+            // @AfterThrowing
             log.info("[트랜잭션 롤백] {}", joinPoint.getSignature());
             throw e;
         } finally {
+            // @After
             log.info("[트랜잭션 릴리즈] {}", joinPoint.getSignature());
         }
     }
