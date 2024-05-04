37a07c54c02571644ee275c8125a12758e77eb2b
LimJiNa <jn4624@gmail.com>
2024-05-05 00:42:54 +0900

    스프링 AOP 주의사항 - 프록시와 내부 호출 문제 대안(지연 조회)

---
+++ src/main/java/hello/aop/internalcall/CallServiceV2.java
@@ -0,0 +1,27 @@
+package hello.aop.internalcall;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Component;
+
+/**
+ * ObjectProvider(Provider), ApplicationContext
+ * 사용하여 지연(LAZY) 조회
+ */
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CallServiceV2 {
+    private final ObjectProvider<CallServiceV2> callServiceProvider;
+
+    public void external() {
+        log.info("call external");
+        CallServiceV2 callServiceV2 = callServiceProvider.getObject();
+        callServiceV2.internal(); // 외부 메서드 호출
+    }
+
+    public void internal() {
+        log.info("call internal");
+    }
+}
